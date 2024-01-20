793f96d38d27583dfbf11d51243af9981e639b28
Tarun Nagar <tarunnagar548@gmail.com>
2024-01-20 16:58:01 -0500

    ASP.NET Lab 2
    LAB 2

---
+++ LabWebApp/LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        // DbSet property for the Product entity
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
+
+
---
+++ LabWebApp/LabWebApp/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<LabWebApp.Data.ApplicationDbContext>(options => {
+    options.UseSqlServer("Server=127.0.0.1;Database=ProductsDb;User=root;Password=123ABC@#;");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
